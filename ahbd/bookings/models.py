c846470b3165ca2a2ab838a687943db71a40e7a5
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-10-04 21:41:31 +0000

    Add lesson and student models

--- ahbd/bookings/models.py
+++ ahbd/bookings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=42)
+    last_name = models.CharField(max_length=42)
+    email = models.EmailField(max_length=75)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def full_name(self):
+        return f"{first_name} {last_name}"
+
+class Lesson(models.Model):
+    lesson_date = models.DateTimeField(auto_now_add=True)
+    plan = models.TextField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['lesson_date']
+
+        def __unicode__(self):
+            return self.lesson_date
+
