5aed00044ab84d0043caee73a164b9874cc4f7a9
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-10-11 21:00:03 +0000

    Format lesson time piped into calendar view

--- ahbd/bookings/views.py
+++ ahbd/bookings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from datetime import datetime
+from bookings.models import Lesson
 
 def calendar(request):
     # Render the weekly calendar view
@@ -7,12 +8,16 @@ def calendar(request):
     for t in range(6,21):
         formatted_time = datetime.strptime(f"{t}:00","%H:%M")
         times.append(formatted_time.strftime("%I:%M %p"))
-
+    def return_formatted_time(lesson):
+        return lesson.lesson_date.strftime("%I:%M %p")
+    lessons = Lesson.objects.all()
+    lesson_times = map(return_formatted_time, lessons)
     return render(
         request,
         "bookings/week.html",
         {
             "times": times,
+            "lessons": list(lesson_times),
             "week": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         }
     )
