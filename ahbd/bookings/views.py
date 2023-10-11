ff19d545a9755c15b59c4f9ab75b12e7fe5c9cbc
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-10-11 17:13:16 +0000

    Generate calendar view timeslots from range of open hours

--- ahbd/bookings/views.py
+++ ahbd/bookings/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def calendar(request):
     # Render the weekly calendar view
+    times = []
+    for t in range(6,21):
+        formatted_time = datetime.strptime(f"{t}:00","%H:%M")
+        times.append(formatted_time.strftime("%I:%M %p"))
+
     return render(
         request,
         "bookings/week.html",
         {
-            "foo": "bar",
+            "times": times,
+            "week": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         }
     )
