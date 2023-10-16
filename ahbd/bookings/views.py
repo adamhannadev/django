c43ac2a854146b744ff3efd59defce36beb8755f
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-10-16 18:03:50 +0000

    Start views.py from scratch

--- ahbd/bookings/views.py
+++ ahbd/bookings/views.py
@@ -1,35 +1,22 @@
 from django.shortcuts import render
 from datetime import datetime, timedelta
-import pandas as pd
 from bookings.models import Lesson
 
 def calendar(request):
     # Render the weekly calendar view
     # Create a list of formatted times for each day
-    times = []
-    for t in range(6,21):
-        formatted_time = datetime.strptime(f"{t}:00","%H:%M")
-        times.append(formatted_time.strftime("%I:%M %p"))
-
-    lessons = Lesson.objects.all()
-    lesson_times = map(return_formatted_time, lessons)
-    formatted_dates = map(return_formatted_time, datelist)
     
-    def build_calendar_blocks(lessons, formatted_dates):
-        blocks = {}
-        blocks['Monday': {
-            lessons[::1]
-        }]
-        return blocks
+    def first_day_of_week(date):
+        # Return the first day of the week of the date passed in
+        return date + datetime.timedelta(days = -date.weekday())
+
+    def build_calendar_blocks():
+        pass
 
     return render(
         request,
         "bookings/week.html",
         {
-            "days": datelist,
-            "lessons": lessons,
-            "times": times,
-            "lesson_times": list(lesson_times),
-            "week": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+            
         }
     )
