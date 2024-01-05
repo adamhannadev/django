8057e653fd1d631757be95575bcbd9b5a2b906be
Adam Hanna <adamhannadev@gmail.com>
2024-01-05 16:02:12 -0700

    Add element type to figures

--- ahbd/progress/models.py
+++ ahbd/progress/models.py
@@ -12,16 +12,23 @@ from django.db import models
 class Level(models.Model):
     level_name = models.CharField(max_length=30)
     level_value = models.IntegerField()
+    def __str__(self):
+        return self.level_name
     
 class Dance(models.Model):
     dance_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.dance_name
 
 class Figure(models.Model):
     figure_name = models.CharField(max_length=30)
+    element = models.CharField(max_length=30)
     leader_description = models.TextField()
     follower_description = models.TextField()
     dance = models.ForeignKey(Dance, on_delete=models.CASCADE)
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.figure_name
 
 
 
